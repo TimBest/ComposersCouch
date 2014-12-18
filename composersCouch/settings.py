06c3851c80cecd6f915ab6bd7955a0d75824418f
Timothy <timbest@composerscouch.com>
2014-12-18 01:12:16 -0500

    some idiot didnt update the database engine

--- composersCouch/settings.py
+++ composersCouch/settings.py
@@ -72,7 +72,7 @@ if DEVELOPMENT:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
