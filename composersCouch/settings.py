fd987e2add90c7a32d1a2dec0259c537be0c7f37
Timothy <timbest@composerscouch.com>
2014-12-19 15:03:37 -0500

    dev to false

--- composersCouch/settings.py
+++ composersCouch/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = 'k$s+jts3d$349yo&ojfqo1wvs!f##2w!p&h$4&qd$uz_5&a7%q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-DEVELOPMENT = True
+DEVELOPMENT = False
 
 TEMPLATE_DEBUG = True
 
@@ -273,8 +273,6 @@ THUMBNAIL_FORMAT = 'PNG'
 # feedly
 CELERY_ALWAYS_EAGER = True
 CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
-
-print redis_url
 
 import djcelery
 djcelery.setup_loader()
