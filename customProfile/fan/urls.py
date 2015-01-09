320a3a690eaaa052eecbb3fa19422f396ef4ca92
Timothy <timbest@composerscouch.com>
2015-01-08 19:27:25 -0500

    fixed fan profile

--- customProfile/fan/urls.py
+++ customProfile/fan/urls.py
@@ -3,19 +3,11 @@ from django.conf.urls import include, patterns, url
 
 view_location = 'customProfile.views'
 urlpatterns = patterns(view_location,
-    # News Tab
-    url(r'^$',
-        'profileRedirect',
-        {'redirect_url': 'fan:news'},
-        name='home'),
-    url(r'^news/$',
-        'fan_news',
-        name='news'),
-    # Photos
-    url(r'^photos/', include(patterns(view_location,
-        url(r'^$',
-            'fan_photos',
-            name='photos'),
+    # Views
+    url(r'^(?P<username>[-\w]+)/', include(patterns(view_location,
+        url(r'^$', 'profileRedirect', {'redirect_url': 'fan:news'}, name='home'),
+        url(r'^news/$', 'fan_news', name='news'),
+        url(r'^photos/$', 'fan_photos', name='photos'),
     ))),
 )
 """
