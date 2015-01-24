3fde71ec6fee71f21272464138c6908cb8219fce
Timothy <timbest@composerscouch.com>
2015-01-24 16:56:34 -0500

    moved media to aws bucket

--- composersCouch/settings.py
+++ composersCouch/settings.py
@@ -149,6 +149,7 @@ if DEVELOPMENT:
     GOOGLE_OAUTH2_CLIENT_SECRET  = 'BpAQ6KT37BLQxNc5ETzC0sMS'
 
     STATIC_URL = '/static/'
+    MEDIA_URL = '/media/'
 
 else:
     ALLOWED_HOSTS = ['*']
@@ -218,15 +219,15 @@ else:
     # We also use it in the next setting.
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-    # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
-    # refers directly to STATIC_URL. So it's safest to always set it.
-    STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-
     # Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
     # you run `collectstatic`).
     STATICFILES_LOCATION = 'static'
     STATICFILES_STORAGE = 'composersCouch.custom_storages.StaticStorage'
     STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+
+    MEDIAFILES_LOCATION = 'media'
+    MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+    DEFAULT_FILE_STORAGE = 'composersCouch.custom_storages.MediaStorage'
 
 SITE_ID = 1
 
@@ -245,8 +246,6 @@ USE_TZ = True
 
 MEDIA_ROOT = os.path.join( BASE_DIR, 'composersCouch/media' )
 #MEDIA_ROOT = '/media/timothy/Elements/test/'
-
-MEDIA_URL = '/media/'
 
 
 #10MB
