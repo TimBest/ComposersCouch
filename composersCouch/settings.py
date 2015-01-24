86fc19fdd77e716819903b103c6df1e9ab601890
Timothy <timbest@composerscouch.com>
2015-01-24 14:48:06 -0500

    getenv not working

--- composersCouch/settings.py
+++ composersCouch/settings.py
@@ -12,17 +12,17 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os, urlparse
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'k$s+jts3d$349yo&ojfqo1wvs!f##2w!p&h$4&qd$uz_5&a7%q')
+#SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'k$s+jts3d$349yo&ojfqo1wvs!f##2w!p&h$4&qd$uz_5&a7%q'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-try:
-    DEVELOPMENT = os.environ['DEVELOPMENT']
-except KeyError:
-    DEVELOPMENT = True
+
+DEVELOPMENT = os.environ['DEVELOPMENT']
+#DEVELOPMENT = True
 
 TEMPLATE_DEBUG = True
 
