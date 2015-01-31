74278ca3e400086845f1bdb10c8b5e5adf9263e5
Timothy <timbest@composerscouch.com>
2015-01-31 18:24:35 -0500

    trying to debug syncdb

--- composersCouch/settings.py
+++ composersCouch/settings.py
@@ -135,8 +135,8 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_HOST = 'smtp-relay.gmail.com'
     EMAIL_PORT = 587
-    EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
-    EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'timbest@composerscouch.com')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
     DEFAULT_FROM_EMAIL = 'messenger@composerscouch.com'
 
     AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
