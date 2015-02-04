b921e5b1b232f7cf4ac8a18d1435a7db8fcd4b2d
Timothy <timbest@composerscouch.com>
2015-02-04 16:42:25 -0500

    added fan and paginated searchs

--- search/views.py
+++ search/views.py
@@ -1,19 +1,33 @@
+from django.db.models import Q
 from django.shortcuts import render
 
-from accounts.models import MusicianProfile, VenueProfile
+from accounts.models import Profile, MusicianProfile, VenueProfile, FanProfile
+from composersCouch.utils import get_page
 
 
 def search(request, template_name='search/search.html', ajax_template='autocomplete/search.html'):
-
-    if request.is_ajax():
-        template_name = ajax_template
-
     q = request.GET.get('q', '')
     context = {'q': q}
 
-    queries = {}
-    queries['musicians'] = MusicianProfile.objects.filter(name__icontains=q)[:3]
-    queries['venues'] = VenueProfile.objects.filter(name__icontains=q)[:3]
+    if request.is_ajax():
+        template_name = ajax_template
+        queries = {}
+        queries['musicians'] = MusicianProfile.objects.filter(name__icontains=q)[:3]
+        queries['venues'] = VenueProfile.objects.filter(name__icontains=q)[:3]
+        queries['fans'] = FanProfile.objects.filter(
+            Q(profile__user__first_name__icontains=q) |
+            Q(profile__user__last_name__icontains=q)
+        )[:3]
+        context.update(queries)
+    else:
+        profiles = Profile.objects.filter(
+            Q(musicianProfile__name__icontains=q) |
+            Q(venueProfile__name__icontains=q) |
+            Q(user__first_name__icontains=q) |
+            Q(user__last_name__icontains=q)
+        )
+        page_num = request.GET.get('page')
+        context['profiles'] = get_page(page_num, profiles, 5)
 
-    context.update(queries)
+
     return render(request, template_name, context)
