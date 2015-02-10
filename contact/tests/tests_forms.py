6c87109f612d502189f0d213ea3c93a2680c7d1c
Timothy <timbest@composerscouch.com>
2015-02-10 13:59:36 -0500

    working contact form tests

--- contact/tests/tests_forms.py
+++ contact/tests/tests_forms.py
@@ -100,7 +100,7 @@ class LinksFormTests(TestCase):
             'error': ('twitter', [_(u'Must be a Twitter URL.')])},
         ]
         for invalid_dict in invalid_data_dicts:
-            form = forms.LocationForm(data=invalid_dict['data'])
+            form = forms.SocialLinksForm(data=invalid_dict['data'])
             self.failIf(form.is_valid())
             self.assertEqual(form.errors[invalid_dict['error'][0]],
                              invalid_dict['error'][1])
@@ -111,7 +111,7 @@ class LinksFormTests(TestCase):
                       'twitter': 'https://twitter.com/thekooksmusic'},},
         ]
         for valid_dict in valid_data_dicts:
-            form = forms.NonUserLocationForm(data=valid_dict['data'])
+            form = forms.SocialLinksForm(data=valid_dict['data'])
             self.failUnless(form.is_valid())
 
     def test_photo_form(self):
@@ -125,7 +125,7 @@ class LinksFormTests(TestCase):
 
         ]
         for invalid_dict in invalid_data_dicts:
-            form = forms.LocationForm(data=invalid_dict['data'])
+            form = forms.PhotoLinksForm(data=invalid_dict['data'])
             self.failIf(form.is_valid())
             self.assertEqual(form.errors[invalid_dict['error'][0]],
                              invalid_dict['error'][1])
@@ -135,7 +135,7 @@ class LinksFormTests(TestCase):
                       'tumblr': 'http://thekooksmusic.tumblr.com/'},},
         ]
         for valid_dict in valid_data_dicts:
-            form = forms.NonUserLocationForm(data=valid_dict['data'])
+            form = forms.PhotoLinksForm(data=valid_dict['data'])
             self.failUnless(form.is_valid())
 
     def test_video_form(self):
@@ -149,7 +149,7 @@ class LinksFormTests(TestCase):
 
         ]
         for invalid_dict in invalid_data_dicts:
-            form = forms.LocationForm(data=invalid_dict['data'])
+            form = forms.VideoLinksForm(data=invalid_dict['data'])
             self.failIf(form.is_valid())
             self.assertEqual(form.errors[invalid_dict['error'][0]],
                              invalid_dict['error'][1])
@@ -159,35 +159,44 @@ class LinksFormTests(TestCase):
                       'vimeo': 'http://vimeo.com/davissilis'},},
         ]
         for valid_dict in valid_data_dicts:
-            form = forms.NonUserLocationForm(data=valid_dict['data'])
+            form = forms.VideoLinksForm(data=valid_dict['data'])
             self.failUnless(form.is_valid())
 
     def test_music_form(self):
         invalid_data_dicts = [
-            {'data': {'facebook': 'composerscouch.com',
-                      'google_plus': '',
-                      'twitter': ''},
-            'error': ('facebook', [_(u'Must be a Facebook URL.')])},
-            {'data': {'facebook': '',
-                      'google_plus': 'composerscouch.com',
-                      'twitter': ''},
-            'error': ('google_plus', [_(u'Must be a Google Plus URL.')])},
-            {'data': {'facebook': '',
-                      'google_plus': '',
-                      'twitter': 'composerscouch.com'},
-            'error': ('twitter', [_(u'Must be a Twitter URL.')])},
+            {'data': {'bandcamp': 'composerscouch.com',
+                      'itunes': '',
+                      'spotify': '',
+                      'soundcloud': ''},
+            'error': ('bandcamp', [_(u'Must be a Bandcamp URL.')])},
+            {'data': {'bandcamp': '',
+                      'itunes': 'composerscouch.com',
+                      'spotify': '',
+                      'soundcloud': ''},
+            'error': ('itunes', [_(u'Must be a iTunes URL.')])},
+            {'data': {'bandcamp': '',
+                      'itunes': '',
+                      'spotify': 'composerscouch.com',
+                      'soundcloud': ''},
+            'error': ('spotify', [_(u'Must be a Spotify URL.')])},
+            {'data': {'bandcamp': '',
+                      'itunes': '',
+                      'spotify': '',
+                      'soundcloud': 'composerscouch.com'},
+            'error': ('soundcloud', [_(u'Must be a SoundCloud URL.')])},
         ]
         for invalid_dict in invalid_data_dicts:
-            form = forms.LocationForm(data=invalid_dict['data'])
+            form = forms.MusicLinksForm(data=invalid_dict['data'])
             self.failIf(form.is_valid())
             self.assertEqual(form.errors[invalid_dict['error'][0]],
                              invalid_dict['error'][1])
 
         valid_data_dicts = [
-            {'data': {'facebook': 'https://www.facebook.com/thekooksofficial',
-                      'google_plus': 'https://plus.google.com/116651435444058665368/about',
-                      'twitter': 'https://twitter.com/thekooksmusic'},},
+            {'data': {'bandcamp': 'http://sekinzer.bandcamp.com/track/junk-of-the-heart-cover',
+                      'itunes': 'https://itunes.apple.com/us/artist/the-kooks/id68448386',
+                      'spotify': 'https://play.spotify.com/artist/1GLtl8uqKmnyCWxHmw9tL4',
+                      'soundcloud': 'https://soundcloud.com/kooksmusic'},},
         ]
         for valid_dict in valid_data_dicts:
-            form = forms.NonUserLocationForm(data=valid_dict['data'])
+            form = forms.MusicLinksForm(data=valid_dict['data'])
             self.failUnless(form.is_valid())
