cf2480e36fa4a0f613d64ac396c91d102f904a7a
Timothy <timbest@composerscouch.com>
2015-02-11 13:51:48 -0500

    added tests for fans

---
+++ fan/tests/tests_views.py
@@ -0,0 +1,27 @@
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+
+class FanViewsTests(TestCase):
+    """  """
+    fixtures = ['users', 'profiles', 'fans']
+
+    def test_home_view(self):
+        """  """
+        user = User.objects.get(pk=1)
+        response = self.client.get(reverse('fan:home', kwargs={'username': user.username}))
+        #redirect to news
+        self.assertEqual(response.status_code, 302)
+
+    def test_news_view(self):
+        """  """
+        user = User.objects.get(pk=1)
+        response = self.client.get(reverse('fan:news', kwargs={'username': user.username}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_photos_view(self):
+        """  """
+        user = User.objects.get(pk=1)
+        response = self.client.get(reverse('fan:photos', kwargs={'username': user.username}))
+        self.assertEqual(response.status_code, 200)
