9e26bb2f3c8b0f8dcdcd7dea8aa3192b10aef6f0
Timothy <timbest@composerscouch.com>
2015-02-11 17:45:06 -0500

    added test for feeds

--- feeds/tests/tests_views.py
+++ feeds/tests/tests_views.py
@@ -13,7 +13,6 @@ class ViewsTests(TestCase):
 
     def test_artist_feed_view(self):
         """  """
-        user = User.objects.get(pk=1)
         orders = ['new', 'all']
         scopes = ['local', 'all',]
         for order in orders:
@@ -57,7 +56,6 @@ class ViewsTests(TestCase):
 
     def test_venue_feed_view(self):
         """  """
-        user = User.objects.get(pk=1)
         orders = ['new', 'all']
         scopes = ['local', 'all',]
         for order in orders:
@@ -104,3 +102,110 @@ class ViewsTests(TestCase):
             response = self.client.get(reverse('venues',
                 kwargs={'order': order,'scope': 'following', 'zipcode': 12065}))
             self.assertEqual(response.status_code, 200)
+
+    def test_request_feed_view(self):
+        """  """
+        requests_for = ['venue', 'band']
+        orders = ['new', 'all', 'expiring']
+        scopes = ['local', 'all',]
+        for request_for in requests_for:
+            for order in orders:
+                for scope in scopes:
+                    response = self.client.get(reverse('requests',
+                        kwargs={'order': order,
+                                'for': request_for,
+                                'scope': scope}))
+                    self.assertEqual(response.status_code, 200)
+                    response = self.client.get(reverse('requests',
+                        kwargs={'order': order,
+                                'for': request_for,
+                                'scope': scope,
+                                'zipcode': 12065}))
+                    self.assertEqual(response.status_code, 200)
+        for request_for in requests_for:
+            for order in orders:
+                response = self.client.get(reverse('requests',
+                    kwargs={'order': order,
+                            'for': request_for,
+                            'scope': 'following'}))
+                self.assertEqual(response.status_code, 302)
+                response = self.client.get(reverse('requests',
+                    kwargs={'order': order,
+                            'for': request_for,
+                            'scope': 'following',
+                            'zipcode': 12065}))
+                self.assertEqual(response.status_code, 302)
+
+        response = self.client.post(reverse('signin'),
+                                    data={'identification': 'jane@example.com',
+                                          'password': 'blowfish'})
+        for request_for in requests_for:
+            for order in orders:
+                response = self.client.get(reverse('requests',
+                    kwargs={'order': order,
+                            'for': request_for,
+                            'scope': 'following'}))
+                self.assertEqual(response.status_code, 200)
+                response = self.client.get(reverse('requests',
+                    kwargs={'order': order,
+                            'for': request_for,
+                            'scope': 'following',
+                            'zipcode': 12065}))
+                self.assertEqual(response.status_code, 200)
+
+    def test_show_feed_view(self):
+        """  """
+        orders = ['new', 'all', 'upcoming']
+        scopes = ['local', 'all',]
+        for order in orders:
+            for scope in scopes:
+                response = self.client.get(reverse('shows',
+                    kwargs={'order': order,'scope': scope}))
+                self.assertEqual(response.status_code, 200)
+                response = self.client.get(reverse('shows',
+                    kwargs={'order': order,'scope': scope, 'zipcode': 12065}))
+                self.assertEqual(response.status_code, 200)
+        for order in orders:
+            response = self.client.get(reverse('shows',
+                kwargs={'order': order,'scope': 'following'}))
+            self.assertEqual(response.status_code, 302)
+            response = self.client.get(reverse('shows',
+                kwargs={'order': order,'scope': 'following', 'zipcode': 12065}))
+            self.assertEqual(response.status_code, 302)
+        response = self.client.post(reverse('signin'),
+                                    data={'identification': 'jane@example.com',
+                                          'password': 'blowfish'})
+        for order in orders:
+            response = self.client.get(reverse('shows',
+                kwargs={'order': order,'scope': 'following'}))
+            self.assertEqual(response.status_code, 200)
+            response = self.client.get(reverse('shows',
+                kwargs={'order': order,'scope': 'following', 'zipcode': 12065}))
+            self.assertEqual(response.status_code, 200)
+
+    def test_updates_feed_view(self):
+        """  """
+        scopes = ['local', 'all',]
+        for scope in scopes:
+            response = self.client.get(reverse('updates',kwargs={'scope': scope}))
+            self.assertEqual(response.status_code, 200)
+            response = self.client.get(reverse('updates',
+                kwargs={'scope': scope, 'zipcode': 12065}))
+            self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(reverse('updates',
+            kwargs={'scope': 'following'}))
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get(reverse('updates',
+            kwargs={'scope': 'following', 'zipcode': 12065}))
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.post(reverse('signin'),
+                                    data={'identification': 'jane@example.com',
+                                          'password': 'blowfish'})
+        response = self.client.get(reverse('updates',
+            kwargs={'scope': 'following'}))
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('updates',
+            kwargs={'scope': 'following', 'zipcode': 12065}))
+        self.assertEqual(response.status_code, 200)
