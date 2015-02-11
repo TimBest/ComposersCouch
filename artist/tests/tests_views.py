3540d19eec346dc20962b03787a7b52dd5e0a3a9
Timothy <timbest@composerscouch.com>
2015-02-11 15:09:31 -0500

    cleared up test case errors

--- artist/tests/tests_views.py
+++ artist/tests/tests_views.py
@@ -7,22 +7,40 @@ from artist.models import ArtistProfile
 from artist.views import MusicianContactsView
 
 
-class AboutViewsTests(TestCase):
+class ViewsTests(TestCase):
     """  """
-    fixtures = ['users', 'profiles', 'artists', 'members']
+    fixtures = ['users', 'profiles', 'artists', 'members', 'calendars']
 
-    def test_about_view(self):
+    def test_view(self):
         """  """
-        user = User.objects.get(pk=1)
+        user = User.objects.get(pk=2)
+        user.profile.artist_profile = ArtistProfile(pk=2)
+        response = self.client.get(reverse('artist:home', kwargs={'username': user.username}))
+        self.assertEqual(response.status_code, 302)
         response = self.client.get(reverse('artist:about', kwargs={'username': user.username}))
         self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('artist:news', kwargs={'username': user.username}))
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('artist:shows', kwargs={'username': user.username}))
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('artist:photos', kwargs={'username': user.username}))
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('artist:music', kwargs={'username': user.username}))
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('artist:videos', kwargs={'username': user.username}))
+        self.assertEqual(response.status_code, 200)
+
+class FormViewsTests(TestCase):
+    """  """
+    fixtures = ['users', 'profiles', 'artists', 'members', 'calendars']
+
+    def setUp(self):
+        response = self.client.post(reverse('signin'),
+                                            data={'identification': 'jane@example.com',
+                                                  'password': 'blowfish'})
+        self.user = User.objects.get(email='jane@example.com')
 
     def test_about_form_views(self):
-        """  """
-        response = self.client.post(reverse('signin'),
-                                    data={'identification': 'john@example.com',
-                                          'password': 'blowfish'})
-        user = User.objects.get(email='john@example.com')
         # biography
         response = self.client.get(reverse('artist:biographyForm'))
         self.assertEqual(response.status_code, 200)
@@ -40,22 +58,7 @@ class AboutViewsTests(TestCase):
             response = self.client.get(reverse('artist:contactForm', kwargs={'contactType': contact_type[0]}))
             self.assertEqual(response.status_code, 200)
 
-class MusicViewsTests(TestCase):
-    """  """
-    fixtures = ['users', 'profiles', 'artists', 'albums']
-
-    def test_music_view(self):
-        """  """
-        user = User.objects.get(pk=1)
-        response = self.client.get(reverse('artist:music', kwargs={'username': user.username}))
-        self.assertEqual(response.status_code, 200)
-
     def test_music_form_views(self):
-        """  """
-        response = self.client.post(reverse('signin'),
-                                    data={'identification': 'john@example.com',
-                                          'password': 'blowfish'})
-        user = User.objects.get(email='john@example.com')
         # album
         response = self.client.get(reverse('artist:albumForm'))
         self.assertEqual(response.status_code, 200)
@@ -66,22 +69,7 @@ class MusicViewsTests(TestCase):
         response = self.client.get(reverse('artist:tracksForm', kwargs={'albumID': 1}))
         self.assertEqual(response.status_code, 200)
 
-class VideosViewsTests(TestCase):
-    """  """
-    fixtures = ['users', 'profiles', 'artists', 'albums']
-
-    def test_videos_view(self):
-        """  """
-        user = User.objects.get(pk=1)
-        response = self.client.get(reverse('artist:music', kwargs={'username': user.username}))
-        self.assertEqual(response.status_code, 200)
-
     def test_videos_form_views(self):
-        """  """
-        response = self.client.post(reverse('signin'),
-                                    data={'identification': 'john@example.com',
-                                          'password': 'blowfish'})
-        user = User.objects.get(email='john@example.com')
         # album
         response = self.client.get(reverse('artist:video_album_form'))
         self.assertEqual(response.status_code, 200)
