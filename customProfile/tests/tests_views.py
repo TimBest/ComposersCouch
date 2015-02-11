a109dc7b15ddcc306bda0298dfc27854cd4a8c30
Timothy <timbest@composerscouch.com>
2015-02-11 13:37:36 -0500

    tests for profile edit

--- customProfile/tests/tests_views.py
+++ customProfile/tests/tests_views.py
@@ -3,10 +3,31 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 
 from accounts.models import Profile
+from artist.models import ArtistProfile
 
 
 class ViewsTests(TestCase):
     """ Test the account views """
+    fixtures = ['users', 'profiles', 'artists', 'venues']
 
-    def test_profile_edit_view(self):
-        pass
+    def setUp(self):
+        # create user and log them in
+        response = self.client.post(reverse('signin'),
+                                    data={'identification': 'john@example.com',
+                                          'password': 'blowfish'})
+        self.user = User.objects.get(email='john@example.com')
+
+    def test_fan_profile_edit_view(self):
+        self.user.profile = Profile.objects.get(pk=1)
+        response = self.client.get(reverse('profile_edit'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_artist_profile_edit_view(self):
+        self.user.profile = Profile.objects.get(pk=2)
+        response = self.client.get(reverse('profile_edit'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_venue_profile_edit_view(self):
+        self.user.profile = Profile.objects.get(pk=3)
+        response = self.client.get(reverse('profile_edit'))
+        self.assertEqual(response.status_code, 200)
