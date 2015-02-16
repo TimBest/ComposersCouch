d7e44e855941958e434df5918ba66d909da26e95
Timothy <timbest@composerscouch.com>
2015-02-16 14:24:01 -0500

    added test for utils

--- tracks/tests/tests_models.py
+++ tracks/tests/tests_models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from request.models import PrivateRequest, RequestParticipant
+from tracks.models import Album, Track
 
 
 class PrivateRequestModelTest(TestCase):
@@ -10,33 +10,7 @@ class PrivateRequestModelTest(TestCase):
                 'threads', 'messages', 'participants', 'dates',
                 'privateRequests', 'requestParticipants']
 
-    # TODO
-    def test_has_accepted(self):
-        private_request = PrivateRequest.objects.get(pk=1)
-        # test that invalid user returns None
-        user_invalid = User.objects.get(pk=1)
-        self.assertEqual(private_request.has_accepted(user_invalid), None)
-        # test that a user that accepted returns true
-        user_accepted = User.objects.get(pk=2)
-        self.assertEqual(private_request.has_accepted(user_accepted), True)
-        # test a user that hasnt responded returns None
-        user_null = User.objects.get(pk=3)
-        self.assertEqual(private_request.has_accepted(user_null), None)
-        # test a user that denied returns false
-
-    def test_headliner(self):
-        #TODO: add this test
+    def test_json_playlist(self):
         pass
-
-    def test_venue(self):
-        self.assertEqual(
-            PrivateRequest.objects.get(pk=1).venue(),
-            RequestParticipant.objects.get(pk=2)
-        )
-
-    def test_openers(self):
-        # TODO: expand to test multiple openers
-        self.assertEqual(
-            PrivateRequest.objects.get(pk=1).openers().first(),
-            RequestParticipant.objects.get(pk=1)
-        )
+        #album = Album.objects.get(pk=1)
+        #tracks = Track.objects.all()
---
+++ tracks/tests/tests_utils.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from tracks.models import Album, Track
+from tracks.utils import json_playlist
+
+
+class PrivateRequestModelTest(TestCase):
+    fixtures = ['users', 'profiles', 'contactInfos','locations', 'contacts',
+                'zipcodes', 'applications', 'publicRequests', 'numApplicants',
+                'threads', 'messages', 'participants', 'dates', 'genres',
+                'albums', 'artists', 'tracks', 'media']
+
+    def test_json_playlist(self):
+        # TODO: add assertion
+        album = Album.objects.get(pk=1)
+        tracks = Track.objects.all()
+        tracks_playlist = json_playlist(tracks)
+        #self.assertEqual(len(list(tracks_playlist)), len(tracks))
+        album_playlist = json_playlist(tracks, album=album)
+        #self.assertEqual(len(list(album_playlist)), len(album.tracks.all()))
