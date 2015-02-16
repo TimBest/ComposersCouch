795b01997013cd775bbaabb2e98276ea43318c1d
Timothy <timbest@composerscouch.com>
2015-02-16 17:00:55 -0500

    tests for thread views

--- threads/tests/tests_views.py
+++ threads/tests/tests_views.py
@@ -3,50 +3,44 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 
 from accounts.models import Profile
-from artist.models import ArtistProfile
-from artist.views import MusicianContactsView
 
 class ViewsTests(TestCase):
     """  """
-    fixtures = ['users', 'profiles', 'artists', 'members', 'calendars']
+    fixtures = ['users', 'profiles', 'contactInfos','locations', 'contacts',
+                'zipcodes', 'applications', 'publicRequests', 'numApplicants',
+                'threads', 'messages', 'participants', 'dates', 'genres',
+                'albums', 'artists', 'tracks', 'media', 'venues']
 
     def test_inbox_views(self):
         """  """
         user = User.objects.get(pk=2)
-        user.profile.artist_profile = ArtistProfile(pk=2)
         url_names = [
-            ['artist:about',  {'username':user.username}],
-            ['artist:news',   {'username':user.username}],
-            ['artist:shows',  {'username':user.username}],
-            ['artist:shows',  {'username':user.username,'year':2015}],
-            ['artist:photos', {'username':user.username}],
-            ['artist:music',  {'username':user.username}],
-            ['artist:videos', {'username':user.username}],
+            ['threads:inbox',   {}],
+            ['threads:sent',    {}],
+            ['threads:trash',   {}],
+            ['threads:compose', {}],
+            ['threads:compose', {'recipient':user.username}],
         ]
 
         for url_name in url_names:
             response = self.client.get(reverse(url_name[0], kwargs=url_name[1]))
-            self.assertEqual(response.status_code, 200)
-        response = self.client.get(reverse('artist:home', kwargs={'username': user.username}))
-        self.assertEqual(response.status_code, 302)
+            self.assertRedirects(response, '%s?next=%s' % (reverse('signin'),
+                                 response.request['PATH_INFO']),
+                                 status_code=302, target_status_code=200,)
 
-    def test_form_permissions_views(self):
-        user = User.objects.get(pk=2)
-        user.profile.artist_profile = ArtistProfile(pk=2)
+        self.client.post(reverse('signin'),
+                                 data={'identification': 'john@example.com',
+                                       'password': 'blowfish'})
+        for url_name in url_names:
+            response = self.client.get(reverse(url_name[0], kwargs=url_name[1]))
+            self.assertEqual(response.status_code, 200)
+
+    def test_thread_views(self):
         url_names = [
-            ['artist:biographyForm',     {}],
-            ['artist:userContactForm',   {}],
-            ['artist:memberForm',        {}],
-            ['artist:memberForm',        {'memberID':1}],
-            ['artist:albumForm',         {}],
-            ['artist:editAlbumForm',     {'albumID':1}],
-            ['artist:tracksForm',        {'albumID':1}],
-            ['artist:video_album_form',  {}],
-            ['artist:video_edit_album',  {'albumID':1}],
-            ['artist:video_tracks_form', {'albumID':1}],
+            ['threads:detail',     {'thread_id':1}],
+            ['threads:delete',     {'thread_id':1}],
+            ['threads:restore',    {'thread_id':1}],
         ]
-        for contact_type in MusicianContactsView.CONTACT_TYPES:
-            url_names.append(['artist:contactForm',{'contactType':contact_type[0]}],)
 
         for url_name in url_names:
             response = self.client.get(reverse(url_name[0], kwargs=url_name[1]))
@@ -69,3 +63,31 @@ class ViewsTests(TestCase):
         for url_name in url_names:
             response = self.client.get(reverse(url_name[0], kwargs=url_name[1]))
             self.assertEqual(response.status_code, 200)
+
+    def test_thread_views(self):
+        url_names = [
+            ['threads:reply',        {'thread_id':1}],
+            ['threads:batch_update', {}],
+        ]
+
+        """for url_name in url_names:
+            response = self.client.get(reverse(url_name[0], kwargs=url_name[1]))
+            self.assertRedirects(response, '%s?next=%s' % (reverse('signin'),
+                                 response.request['PATH_INFO']),
+                                 status_code=302, target_status_code=200,)
+        # user with out permission is denied
+        self.client.post(reverse('signin'),
+                                 data={'identification': 'john@example.com',
+                                       'password': 'blowfish'})
+        for url_name in url_names:
+            response = self.client.get(reverse(url_name[0], kwargs=url_name[1]))
+            self.assertEqual(response.status_code, 403)
+        self.client.logout()
+
+        # user with permission is redirected
+        self.client.post(reverse('signin'),
+                                 data={'identification': 'jane@example.com',
+                                       'password': 'blowfish'})
+        for url_name in url_names:
+            response = self.client.get(reverse(url_name[0], kwargs=url_name[1]))
+            self.assertEqual(response.status_code, 200)"""
