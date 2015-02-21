bee379e1ea5263f253b63c0ce01c6f937634961f
Timothy <timbest@composerscouch.com>
2015-02-20 21:28:26 -0500

    added some post testing on artists

--- artist/tests/tests_views.py
+++ artist/tests/tests_views.py
@@ -8,7 +8,36 @@ from artist.views import MusicianContactsView
 
 class ViewsTests(TestCase):
     """  """
-    fixtures = ['users', 'contactInfos', 'contacts', 'locations', 'zipcodes', 'profiles', 'artists', 'members', 'calendars']
+    fixtures = ['users', 'contactInfos', 'contacts', 'locations', 'zipcodes',
+                'profiles', 'artists', 'members', 'calendars']
+
+    def _test_only_viewable_by_artist(self, url_name, kwargs):
+        # Anonymous user is redirected to login
+        self.client.logout()
+        response = self.client.get(reverse(url_name, kwargs=kwargs))
+        self.assertRedirects(response, '%s?next=%s' % (reverse('signin'),
+                             response.request['PATH_INFO']), status_code=302,
+                             target_status_code=200,)
+        # Non Artist is given permission denied
+        self.client.post(reverse('signin'),
+                         data={'identification': 'john@example.com',
+                               'password': 'blowfish'})
+        response = self.client.get(reverse(url_name, kwargs=kwargs))
+        self.assertEqual(response.status_code, 403)
+        self.client.logout()
+        # Artist is given the page
+        self.client.post(reverse('signin'),
+                         data={'identification': 'jane@example.com',
+                               'password': 'blowfish'})
+        response = self.client.get(reverse(url_name, kwargs=kwargs))
+        self.assertEqual(response.status_code, 200)
+
+    def _signin_artist(self):
+        self.client.logout()
+        self.client.post(reverse('signin'),
+                         data={'identification': 'jane@example.com',
+                               'password': 'blowfish'})
+
 
     def test_view(self):
         """  """
@@ -30,12 +59,68 @@ class ViewsTests(TestCase):
         response = self.client.get(reverse('artist:home', kwargs={'username': user.username}))
         self.assertEqual(response.status_code, 302)
 
+
+    def test_biography_form_view(self):
+        url_name = 'artist:biographyForm'
+        self._test_only_viewable_by_artist(url_name, {})
+        values = {
+            'biography': 'This is a sample biography. This is a\
+                          second sentance about myself. And this is a Third.\
+                          Now i will go on to describe another thing about my\
+                          self. Hopefully this is not to long.',
+        }
+        self._signin_artist()
+        response = self.client.post(reverse(url_name), values, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.context["user"].profile.artist_profile.biography,
+            values['biography']
+        )
+
+    def test_contact_form_view(self):
+        user = None
+        values = {
+            'name': 'Santi White',
+            'email': 'santigold@example.com',
+            'phone': '8765309',
+            'url': 'www.example.com',
+            'address_1': '3 Zamora Court',
+            'address_2': 'apt. 215',
+            'city': 'Clifton Park',
+            'state': 'NY',
+            'zip_code': 12065,
+        }
+        url_names = [['artist:userContactForm', {}],]
+        for contact_type in MusicianContactsView.CONTACT_TYPES:
+            url_names.append(['artist:contactForm',{'contactType':contact_type}],)
+        self._signin_artist()
+        for url_name in url_names:
+            self._test_only_viewable_by_artist(url_name[0], url_name[1])
+            response = self.client.post(
+                reverse(url_name[0], kwargs=url_name[1]), values, follow=True
+            )
+            user = response.context["user"]
+            self.assertEqual(response.status_code, 200)
+            self.assertNotEqual(
+                reverse(url_name[0], kwargs=url_name[1]),
+                response.request['PATH_INFO']
+            )
+        self.assertEqual(
+            user.profile.contact_info.contact.name,
+            values['name']
+        )
+
+        for contact_type in MusicianContactsView.CONTACT_TYPES:
+            contact_info = getattr(
+                user.profile.artist_profile,
+                MusicianContactsView.CONTACT_TYPES[contact_type]
+            )
+            self.assertEqual(contact_info.contact.name, values['name'])
+
     def test_form_permissions_views(self):
         user = User.objects.get(pk=2)
         user.profile.artist_profile = ArtistProfile(pk=2)
         url_names = [
-            ['artist:biographyForm',     {}],
-            ['artist:userContactForm',   {}],
             ['artist:memberForm',        {}],
             ['artist:memberForm',        {'memberID':1}],
             ['artist:albumForm',         {}],
@@ -45,8 +130,6 @@ class ViewsTests(TestCase):
             ['artist:video_edit_album',  {'albumID':1}],
             ['artist:video_tracks_form', {'albumID':1}],
         ]
-        for contact_type in MusicianContactsView.CONTACT_TYPES:
-            url_names.append(['artist:contactForm',{'contactType':contact_type[0]}],)
 
         for url_name in url_names:
             response = self.client.get(reverse(url_name[0], kwargs=url_name[1]))
