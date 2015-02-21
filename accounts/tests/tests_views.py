5debb0bee0d7e6a485da9d3d5a57606fbd126e40
Timothy <timbest@composerscouch.com>
2015-02-20 19:29:10 -0500

    added some test for view posts

--- accounts/tests/tests_views.py
+++ accounts/tests/tests_views.py
@@ -7,7 +7,8 @@ from accounts.models import Profile
 
 class ViewsTests(TestCase):
     """ Test the account views """
-    fixtures = ['users', 'contactInfos', 'contacts', 'locations', 'zipcodes', 'profiles']
+    fixtures = ['users', 'contactInfos', 'contacts', 'locations', 'zipcodes',
+                'profiles', 'artists', 'venues', 'fans',]
     claim_profile = Profile(user=User(pk=1), has_owner=False)
 
     def test_signup_auth_view(self):
@@ -19,9 +20,29 @@ class ViewsTests(TestCase):
         """ """
         response = self.client.get(reverse('signup_email'))
         self.assertEqual(response.status_code, 200)
+        valid_values = [
+            {'email': 'signUpEmailArtist@example.com',
+             'password1': 'foobar',
+             'password2': 'foobar',
+             'profile_type': 'm',
+             'zip_code': 12065,
+             'first_name': '',
+             'last_name': '',
+             'band_name': 'Band Name',
+             'venue_name': ''},
+        ]
+        #TODO:: find out how to pass profile type to the view
+        for values in valid_values:
+            response = self.client.post(reverse('signup_email'), values, follow=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(
+                Profile.objects.get(user__email=values['email']).profile_type,
+                values['profile_type']
+            )
 
     def test_signup_social_view(self):
         """ """
+        # TODO: test POST
         response = self.client.get(reverse('signupSocial'))
         self.assertEqual(response.status_code, 200)
 
@@ -38,21 +59,25 @@ class ViewsTests(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_claim_profile_confirm_view(self):
-        """
-        TODO: generate token and test this
-
-        """
+        """        """
+        # TODO: generate token and test this
         pass
 
     def test_signin_view(self):
-        """
-        TODO: A valid ``POST`` to the signin view should redirect the user to it's
-        own profile page if no ``next`` value is supplied. Else it should
-        redirect to ``next``.
-
-        """
+        """        """
         response = self.client.get(reverse('signin'))
         self.assertEqual(response.status_code, 200)
+        valid_values = [
+            {'identification': 'john@example.com',
+             'password': 'blowfish',},
+        ]
+        for values in valid_values:
+            response = self.client.post(reverse('signin'), values, follow=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(
+                User.objects.get(email=values['identification']),
+                response.context["user"]
+            )
 
     def test_loginredirect_view(self):
         """ A ``GET`` to the signout view """
