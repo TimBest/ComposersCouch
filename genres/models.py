dcf674a54572156dc5eea988e6b852c111cea8cd
Timothy <timbest@composerscouch.com>
2015-02-23 21:20:53 -0500

    added popular tag to categories

--- genres/models.py
+++ genres/models.py
@@ -6,14 +6,21 @@ class Genre(models.Model):
     name =  models.CharField(_("name"), max_length=64)
     slug =  models.CharField(_("slug"), max_length=64)
 
+    class Meta:
+        ordering = ['name']
+
     def __unicode__(self):
         return u'{0}'.format(self.name)
 
 class Category(models.Model):
     name =  models.CharField(_("name"), max_length=64)
     slug =  models.CharField(_("slug"), max_length=64)
+    popular =  models.BooleanField(_("popular"), default=False)
     genres = models.ManyToManyField(Genre, verbose_name=_("genres"),
                                related_name='categories')
+
+    class Meta:
+        ordering = ['name']
 
     def __unicode__(self):
         return u'{0}'.format(self.name)
