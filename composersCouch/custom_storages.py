9750fe23758c0d6421ccc42ca235e74f2f37b413
Timothy <timbest@composerscouch.com>
2015-03-01 17:59:36 -0500

    enabled gzip

--- composersCouch/custom_storages.py
+++ composersCouch/custom_storages.py
@@ -1,11 +1,11 @@
 from django.conf import settings
 from django.contrib.staticfiles.storage import CachedFilesMixin
 
-from pipeline.storage import PipelineMixin
+from pipeline.storage import GZIPMixin PipelineMixin
 from storages.backends.s3boto import S3BotoStorage
 
 
-class StaticStorage(PipelineMixin, S3BotoStorage):
+class StaticStorage(GZIPMixin, PipelineMixin, S3BotoStorage):
     location = settings.STATICFILES_LOCATION
 
 class MediaStorage(S3BotoStorage):
