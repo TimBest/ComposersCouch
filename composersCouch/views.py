924a027635a254d641c08be7c79323d897f530ed
Timothy <timbest@composerscouch.com>
2015-04-07 18:00:45 -0700

    integrted landing age into feeds and adding in sign up form

--- composersCouch/views.py
+++ composersCouch/views.py
@@ -1,9 +1,10 @@
 from django.views.generic.base import TemplateView
 
+from accounts.views import SignupEmailView
 from feeds.views import ShowView
 
 
-class LandingPage(ShowView):
+class LandingPage(SignupEmailView, ShowView):
     template_name = "landing_page.html"
 
 landing_page = LandingPage.as_view()
