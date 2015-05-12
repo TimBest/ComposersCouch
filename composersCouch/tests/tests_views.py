e3162cd7f6463b468a61d7174b4fc9fd85167e3c
Timothy <timbest@composerscouch.com>
2015-05-12 11:57:03 -0700

    added tests for new static views

--- composersCouch/tests/tests_views.py
+++ composersCouch/tests/tests_views.py
@@ -4,17 +4,30 @@ from django.test import TestCase
 
 class TemplateViewsTests(TestCase):
     """  """
+    fixtures = ['users', 'contactInfos', 'contacts', 'locations', 'zipcodes',
+                'profiles', 'artists', 'members', 'calendars', 'albums',
+                'media', 'tracks','genres']
 
     def test_about_view(self):
-        # Home page
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
-        # About
-        response = self.client.get(reverse('about'))
-        self.assertEqual(response.status_code, 200)
-        # Team
-        response = self.client.get(reverse('team'))
-        self.assertEqual(response.status_code, 200)
-        # Credit
-        response = self.client.get(reverse('credit'))
-        self.assertEqual(response.status_code, 200)
+        url_names = [
+            ['home',  {}],
+            ['about',  {}],
+            ['team',  {}],
+            ['credit',  {}],
+            ['learn',  {}],
+            ['changelog',  {}],
+            ['pipeline',  {}],
+            ['sitemap',  {}],
+            ['robots_rule_list',  {}],
+        ]
+
+        for url_name in url_names:
+            response = self.client.get(reverse(url_name[0], kwargs=url_name[1]))
+            self.assertEqual(response.status_code, 200)
+        self.client.logout()
+        self.client.post(reverse('login'),
+                         data={'identification': 'jane@example.com',
+                               'password': 'blowfish'})
+        for url_name in url_names:
+            response = self.client.get(reverse(url_name[0], kwargs=url_name[1]))
+            self.assertEqual(response.status_code, 200)
