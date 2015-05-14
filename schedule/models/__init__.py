dc3e5c6137d2ed8dda97ca80e8d0d874237b8339
Timothy <timbest@composerscouch.com>
2015-05-14 16:13:04 -0700

    removed old import of signals

--- schedule/models/__init__.py
+++ schedule/models/__init__.py
@@ -2,5 +2,3 @@ from schedule.models.calendars import *
 from schedule.models.info import *
 from schedule.models.events import *
 from schedule.models.lines import *
-
-from schedule.signals import *
