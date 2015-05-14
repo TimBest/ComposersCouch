a87ce03345e1343f78ec2ae6b8f2592073cade4e
Timothy <timbest@composerscouch.com>
2015-05-13 17:40:51 -0700

    fixed javascript for music page

--- composersCouch/staticfiles/js/forms.js
+++ composersCouch/staticfiles/js/forms.js
@@ -166,3 +166,27 @@ $(document).ready(function() {
     show_participants("id_openers", "artist-autocomplete");
   }
 });
+
+// Track Player Script
+$(document).ready(function() {
+  $('.json_playlist').each( function (index, data) {
+      var id = $(this).attr('id');
+      var playlist = jQuery.parseJSON($(this).text());
+      var myPlaylist = new jPlayerPlaylist({
+        jPlayer: "#jquery_jplayer_"+id,
+        cssSelectorAncestor: "#jp_container_"+id
+      },
+      playlist,
+      {
+        playlistOptions: {
+          enableRemoveControls: false
+        },
+        swfPath: "/js",
+        supplied: "ogv, m4v, oga, mp3",
+        keyEnabled: true,
+        displayTime: 0,
+        audioFullScreen: false // Allows the audio poster to go full screen via keyboard
+      });
+      $('#jquery_jplayer_'+id).hide();
+  });
+});
