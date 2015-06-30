0d7b5612725542d550ab2d66641339973ee325b3
Timothy <timbest@composerscouch.com>
2015-06-30 16:00:05 -0400

    updated get zipcode

--- contact/utils.py
+++ contact/utils.py
@@ -1,5 +1,6 @@
 from annoying.functions import get_object_or_None
 from contact.models import Zipcode
+from easy_timezones.utils import get_ip_address_from_request
 from easy_timezones import middleware
 
 
@@ -10,7 +11,7 @@ def get_location(request, code=None, attr='code'):
     elif hasattr(request, 'user') and request.user.is_authenticated():
         zipcode = request.user.profile.contact_info.location.zip_code
     else:
-        ip = middleware.get_ip_address_from_request(request)
+        ip = get_ip_address_from_request(request)
         if ip == '127.0.0.1':
             ip = "96.249.1.67"
         if not middleware.db_loaded:
